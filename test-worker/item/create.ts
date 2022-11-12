ada8c3fa6d5796cbe47973de703cca357ef5c375
Marcus Hanses <marcus.hanses@gmail.com>
2022-11-12 21:21:35 +0100

    Fixed linting.

--- test-worker/item/create.ts
+++ test-worker/item/create.ts
@@ -1,10 +1,10 @@
-import { router } from "../router"
 import * as gracely from "gracely"
 import * as model from "../model"
+import { router } from "../router"
 
 router.add({
 	listen: { methods: "POST", pattern: "/item" },
 	authenticate: async request => request.header.authorization,
 	body: model.Item.is,
-	result: (request) => gracely.success.created(request.body),
+	result: request => gracely.success.created(request.body),
 })
